fbed1f76b55e061e680a2b5cb4ac2c48351c94a6
Gökhan Ayrancıoğlu <gkhan.a.06@gmail.com>
2022-08-22 21:34:29 +0300

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,4 +1,6 @@
-require('dotenv').config();
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
 module.exports = {
   siteMetadata: {
     siteTitle: 'Gökhan Ayrancıoğlu - Software Engineer, Blogger, Community Organizer',
